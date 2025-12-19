79180bca2e4e289e450929e7db787e5b9555396f
EDilleHSC <dilledailing@gmail.com>
2025-12-19 16:47:04 -0700

    test(ci): add approval handler, unit+integration tests and CI workflow; update jest config & deps

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testMatch: ['**/tests/unit/**/*.test.[jt]s?(x)'],
+  testMatch: ['**/tests/unit/**/*.test.[jt]s?(x)','**/tests/integration/**/*.test.[jt]s?(x)'],
   setupFiles: ['<rootDir>/jest.setup.js'],
   verbose: true,
 };
